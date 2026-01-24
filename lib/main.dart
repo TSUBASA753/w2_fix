fecd231170a1c7f70b24f6cafcd3a26b935a6368
khomsak <khomsak.chowwiang@e-tech.ac.th>
2026-01-24 11:58:30 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,28 +8,31 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Cal',
+
+      title: 'EiEi',
       theme: ThemeData(
+
 
         appBarTheme: AppBarTheme(
             backgroundColor: Color(0xFFE0AFA0),
-            foregroundColor: Color(0xFF8A817C)
+            foregroundColor: Color(0xFFFF7300)
         ),
         colorScheme: ColorScheme.fromSeed(seedColor: Color(0x00000000)),
         scaffoldBackgroundColor: const Color(0xF0F3E6D9),
-        ),
+      ),
       routes: {
         '/' : (context) => MyHomePage(),
+        '/c' : (context) => CalPage(),
         '/rtg' : (context) => RectanglePage() // rtg = Rectsngle
       },
     );
   }
 }
+
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
@@ -38,14 +41,47 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'โปรแกรมคำนวณหาพื้นที่สี่เหลี่ยม',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w100),
+            ),
+            const SizedBox(height: 20),
 
-        child: TextButton(onPressed: () => Navigator.pushNamed(context, '/rtg' ),
-          child: Text('Start'),
-        style: TextButton.styleFrom(
-          backgroundColor: Color(0xFFE0AFA0),
-          foregroundColor: Color(0xFF8A817C)
-
-         ),
+            const SizedBox(height: 25), // เพิ่มระยะห่าง
+            TextButton(
+              onPressed: () => Navigator.pushNamed(context, '/rtg'),
+              style: TextButton.styleFrom(
+                backgroundColor: const Color(0xFFE0AFA0),
+                foregroundColor: const Color(0xFFFF7300),
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 30, vertical: 15),
+              ),
+              child: const Text('Start', style: TextStyle(fontSize: 18)),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'โปรแกรมคำนวณ',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w100),
+            ),
+            const SizedBox(height: 20),
+            const Icon(Icons.percent_sharp, // icon %
+              size: 50,
+            ),
+            const SizedBox(height: 25), // เพิ่มระยะห่าง
+            TextButton(
+              onPressed: () => Navigator.pushNamed(context, '/c'),
+              style: TextButton.styleFrom(
+                backgroundColor: const Color(0xFFE0AFA0),
+                foregroundColor: const Color(0xFFFF7300),
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 30, vertical: 15),
+              ),
+              child: const Text('Start', style: TextStyle(fontSize: 18)),
+            ),
+          ],
         ),
       ),
     );
